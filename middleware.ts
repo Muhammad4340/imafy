34fdab6f01057fd3c114ebfb4cffaed7fa94c7c9
Muhammad4340 <68977832+Muhammad4340@users.noreply.github.com>
2024-12-24 05:27:58 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,17 @@
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/webhooks/clerk",
-    "/api/webhooks/stripe",
-    "/transformations/add/recolor",
-    "/transformations/add/remove",
-    "/transformations/add/fill",
-    "/transformations/add/restore",
-    "/transformations/add/removeBackground",
-    "/profile",
-    "/credits",
-  ],
-  debug: true,
-  beforeAuth: async (req, evt) => {
-    console.log("Cookies in request:", req.headers.get("cookie"));
-  },
-});
+import { clerkMiddleware } from "@clerk/nextjs/server"
+import { NextRequest } from "next/server";
 
+export default clerkMiddleware(async (auth, request) => {
+  function isPublicRoute(request: NextRequest) {
+    const publicRoutes = ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'];
+    return publicRoutes.includes(request.url);
+  }
 
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
+ 
 export const config = {
-  matcher: ["/", "/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
