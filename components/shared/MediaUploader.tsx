05000c84d256e11aba0076255e62cc669375a0d5
Muhammad4340 <rafibaureno2@gmail.com>
2024-12-23 16:57:04 +0700

    revised

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="jsm_imafy"
       options={{
         multiple: false,
         resourceType: "image",
