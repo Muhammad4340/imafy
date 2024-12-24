bcc58290d5738d5be594dba3856730a1c5702318
Muhammad4340 <68977832+Muhammad4340@users.noreply.github.com>
2024-12-24 15:23:37 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe', '/transformations(.*)'],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\\.[\\w]+$)"], // Abaikan file statis
+  debug: true, // Aktifkan debug untuk log detail
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"]
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
