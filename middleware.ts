b945e24e4910eeda0d26daf372e53849c5c32f28
Muhammad4340 <68977832+Muhammad4340@users.noreply.github.com>
2024-12-24 14:08:36 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"]
+  export default authMiddleware({
+  debug: true,
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe', '/transformations(.*)'],
 });
- 
+   
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
